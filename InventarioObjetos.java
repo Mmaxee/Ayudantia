948e95c0b52e3f114a41379399c6aae98bc13332
Mmaxee <37594928+Mmaxee@users.noreply.github.com>
2018-04-08 16:24:04 -0300

    Add files via upload

---
+++ InventarioObjetos.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class inventarioObjetos {
+
+	private int maxItems = 10;
+	
+	public void agregarItem(ArrayList<objetoEquipable> item) {
+		if (item.size()<maxItems) {
+		item.add(new objetoEquipable());
+		}
+		else {
+			System.out.println("Inventario lleno");
+		}
+	}
+	public void quitarItem(ArrayList<objetoEquipable> item) {
+		System.out.println("Que item desea eliminar, guíese por el [x]");
+		mostrarItems(item);
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		item.remove(i-1);
+	}
+	
+	public void mostrarItems(ArrayList<objetoEquipable> item) {
+		for(int i=0; i<item.size();i++) {
+			System.out.println("Item ["+(i+1)+"]");
+			item.get(i).mostrarObjeto();
+		}
+	}
+	
+	public void filtroRarity(ArrayList<objetoEquipable> item) {
+		ArrayList<objetoEquipable> Rarity = new ArrayList<>();
+		int aux = 0 ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Filtrar por x estrellas, guíese por [x]:");
+    	aux = sc.nextInt();
+    	
+    		for(int i=0; i<item.size(); i++) {
+    			if(item.get(i).getRarity() == aux) {
+    				Rarity.add(item.get(i));
+    				}
+    			}
+    		for(int i=0; i<Rarity.size();i++) {
+    			System.out.println("Item ["+(i+1)+"]");
+    			Rarity.get(i).mostrarObjeto();	
+    				}
+    	
+		}
+}
---
+++ Monstruo.java
@@ -0,0 +1,19 @@
+
+public class Monstruo {
+
+	private int atk,def,spd,hp;
+	
+	
+	public Monstruo() {
+		this.atk = (int) (Math.random()*4000+3500);
+		this.def = (int) (Math.random()*1500+1000);
+		this.def = (int) (Math.random()*25+5);
+		this.spd = (int) (Math.random()*100*25);
+	}
+	public void crearObjetoDropeable() {
+		objetoEquipable drop = new objetoEquipable();
+		System.out.println("Item drop:");
+		drop.mostrarObjeto();
+	}
+	
+}
