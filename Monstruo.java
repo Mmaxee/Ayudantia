a8b26f67825594e51a4d486f9dfccca19ecee308
Mmaxee <37594928+Mmaxee@users.noreply.github.com>
2018-04-12 17:40:46 -0300

    Update Monstruo.java

--- Monstruo.java
+++ Monstruo.java
@@ -8,18 +8,15 @@ public class Monstruo {
 		crearMonstruo();
 	}
 	
-		
 	public void crearMonstruo() {
 		this.atk = (int) (Math.random()*4000+3500);
 		this.def = (int) (Math.random()*1500+1000);
 		this.def = (int) (Math.random()*25+5);
 		this.spd = (int) (Math.random()*100*25);
 	}
-	
 	public void crearObjetoDropeable() {
 		objetoEquipable drop = new objetoEquipable();
 		System.out.println("Item drop:");
 		drop.mostrarObjeto();
 	}
-	
 }
