c58de428123ebe645886fc2b55c6ecf611e7444c
Mmaxee <37594928+Mmaxee@users.noreply.github.com>
2018-04-15 21:37:04 -0300

    Update InventarioObjetos.java

--- InventarioObjetos.java
+++ InventarioObjetos.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class inventarioObjetos {
 
 	private int maxItems = 10;
+	private ArrayList<objetoEquipable> item = new ArrayList<>();
 	
-	public void agregarItem(ArrayList<objetoEquipable> item) {
+	public void agregarItem() {
 		if (item.size()<maxItems) {
 		item.add(new objetoEquipable());
 		}
@@ -13,26 +13,26 @@ public class inventarioObjetos {
 			System.out.println("Inventario lleno");
 		}
 	}
-	public void quitarItem(ArrayList<objetoEquipable> item) {
-		System.out.println("Que item desea eliminar, guíese por el [x]");
-		mostrarItems(item);
+	public void quitarItem() {
+		System.out.println("Que item desea eliminar, guÃ­ese por el [x]");
+		mostrarItems();
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt();
 		item.remove(i-1);
 	}
 	
-	public void mostrarItems(ArrayList<objetoEquipable> item) {
+	public void mostrarItems() {
 		for(int i=0; i<item.size();i++) {
 			System.out.println("Item ["+(i+1)+"]");
 			item.get(i).mostrarObjeto();
 		}
 	}
 	
-	public void filtroRarity(ArrayList<objetoEquipable> item) {
+	public void filtroRarity() {
 		ArrayList<objetoEquipable> Rarity = new ArrayList<>();
 		int aux = 0 ;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Filtrar por x estrellas, guíese por [x]:");
+		System.out.println("Filtrar por x estrellas, guÃ­ese por [x]:");
     	aux = sc.nextInt();
     	
     		for(int i=0; i<item.size(); i++) {
